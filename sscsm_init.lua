41e0d803d5b69bb3360f84579ce6182da1636a5e
luk3yx <luk3yx@users.noreply.github.com>
2020-11-26 14:00:39 +1300

    Change license to LGPL 3.0+

--- sscsm_init.lua
+++ sscsm_init.lua
@@ -2,8 +2,21 @@
 -- SSCSM: Server-Sent Client-Side Mods proof-of-concept
 -- Initial code sent to the client
 --
--- Copyright © 2019 by luk3yx
--- License: https://luk3yx.mit-license.org/@2019
+-- Copyright © 2019-2020 by luk3yx
+-- License: https://git.minetest.land/luk3yx/sscsm/src/branch/master/LICENSE.md
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 --
 
 -- Make sure both table.unpack and unpack exist.
